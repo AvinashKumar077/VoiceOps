b7c8a0055c44fcc9789ec1b77d857a9386cbc977
avinashkumar <avinash.kumar@refex.co.in>
2026-07-07 16:41:06 +0530

    ADDED: preprocessing done

--- app/api/upload.py
+++ app/api/upload.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter, UploadFile, File
+import time
 
 from app.services.upload_service import UploadService
-
-import time
 
 router = APIRouter()
 
@@ -12,6 +11,9 @@ service = UploadService()
 @router.post("/upload")
 async def upload(file: UploadFile = File(...)):
     start = time.perf_counter()
+
     conversations = service.upload(file.file)
+
     print(f"Total: {time.perf_counter() - start:.3f}s")
+
     return conversations
--- app/services/upload_service.py
+++ app/services/upload_service.py
@@ -1,10 +1,14 @@
 from app.parsers.csv_parser import CsvParser
+from app.preprocessing.preprocessing_pipeline import PreprocessingPipeline
 
 
 class UploadService:
 
     def __init__(self):
         self.csv_parser = CsvParser()
+        self.pipeline = PreprocessingPipeline()
 
     def upload(self, file):
-        return self.csv_parser.parse(file)
+        conversations = self.csv_parser.parse(file)
+
+        return self.pipeline.process(conversations)
